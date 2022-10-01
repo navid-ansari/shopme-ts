e8b0ea12be1d91ccf3159ac5969f7a1ce239a04d
navid-ansari <navidansari25july@gmail.com>
2022-10-02 02:22:45 +0530

    axios client unit test added

--- src/utils/axios-rest-client.ts
+++ src/utils/axios-rest-client.ts
@@ -1,36 +1,13 @@
 import axios, { AxiosError } from 'axios'
 
-import {
-  InputError,
-  AuthenticationError,
-  ForbiddenError,
-  NotFoundError,
-  InternalServerError,
-  BadResponseError
-} from './error-handler'
+import { throwError } from './throw-error'
 
 export const axiosGet = async (url: string) => {
   try {
     const response = await axios.get<any>(url)
     return response
   } catch (error: any) {
-    const err = error as AxiosError
-    throwError({ status: err?.response?.status || 404 })
-  }
-}
-
-export const throwError = ({ status }: { status: number }) => {
-  if (status === 400) {
-    throw new InputError('Bad client request')
-  } else if (status === 401) {
-    throw new AuthenticationError('Unauthorized request')
-  } else if (status === 403) {
-    throw new ForbiddenError('Forbidden request')
-  } else if (status === 404) {
-    throw new NotFoundError('Invalid client request url')
-  } else if (status === 500) {
-    throw new InternalServerError('Internal server error')
-  } else if (status === 502) {
-    throw new BadResponseError('Invalid response from gateway server')
+    const err: any = error as AxiosError
+    throwError({ status: err?.response?.status })
   }
 }
