72f82296769c0682a44cb098a261c9e33912c00d
navid-ansari <navidansari25july@gmail.com>
2023-12-24 03:43:01 +0530

    E2E test fixed

--- cypress/e2e/tests/home.cy.ts
+++ cypress/e2e/tests/home.cy.ts
@@ -2,24 +2,24 @@ describe('E2E: homepage', () => {
   beforeEach(() => {
     cy.visit('localhost:3000')
     cy.get('#header').should('exist')
-    cy.get('.productlist-page').should('exist')
+    cy.get('.product-list-page').should('exist')
     cy.get('.footer').should('exist')
   })
 
   it('check click on first product', () => {
-    cy.get('.content').first().click()
+    cy.get('.product').first().click()
   })
 
   it('check favorite icon click on first product', () => {
-    cy.get('.content .ri-heart-line').first().click()
+    cy.get('.product .ri-heart-line').first().click()
   })
 
   it('check add to cart icon click on first product', () => {
-    cy.get('.content .add-to-cart').first().click()
+    cy.get('.product .add-to-cart').first().click()
   })
 
   it('check favorite and add to cart icon click on first product', () => {
-    cy.get('.content .ri-heart-line').first().click()
-    cy.get('.content .add-to-cart').first().click()
+    cy.get('.product .ri-heart-line').first().click()
+    cy.get('.product .add-to-cart').first().click()
   })
 })
