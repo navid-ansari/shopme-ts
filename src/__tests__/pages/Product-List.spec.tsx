76b92224ec57cd9fcee77ad0795ee7093bc88144
navid-ansari <navidansari25july@gmail.com>
2024-02-21 01:48:06 +0530

    real redux store in UT

--- src/__tests__/pages/Product-List.spec.tsx
+++ src/__tests__/pages/Product-List.spec.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { act, fireEvent, screen } from '@testing-library/react'
 import { MemoryRouter as Router } from 'react-router-dom'
 
 import { renderComponent } from '../test-utils/component-renderer'
@@ -9,18 +9,19 @@ import Product from '../../components/Product'
 // mocks
 import MockedProducts from '../mocks/products'
 import { IProduct } from '../types/Product'
+import axios from 'axios'
+import { getApiUrl } from '../../utils/get-api-url'
+import { Provider } from 'react-redux'
+import store from '../../redux/store'
+
+jest.mock('axios')
 
 describe('Product List page', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>
   let mockedProducts = [] as IProduct[]
   beforeAll(() => {})
   beforeEach(() => {
     mockedProducts = MockedProducts()
-    /*const renderWithRouter = (ui, { route = "/" } = {}) => {
-   window.history.pushState({}, "Test page", route);
-
-   return render(ui, { wrapper: BrowserRouter });
-  };
-  renderWithRouter(<Product />);*/
   })
   afterEach(() => {})
   afterAll(() => {})
@@ -41,12 +42,42 @@ describe('Product List page', () => {
       <Product key={product.id} product={product} toggleFavorite={mockOnClick} toggleCart={mockOnClick} />
     ))
     const wrapper = await renderComponent(<Router>{productsElem}</Router>)
-    //const content = screen.queryAllByTestId("content");
-    //expect(content).toHaveLength(3);
 
     const productItems = screen.getAllByTestId('product')
     expect(productItems).toHaveLength(3)
 
     await wrapper.unmount()
+  })
+
+  test('check add to cart button', async () => {
+    const mAxiosResponse = {
+      data: mockedProducts,
+      status: 200,
+      headers: {
+        'content-type': 'application/json; charset=utf-8'
+      },
+      statusText: '',
+      config: {
+        url: getApiUrl('products')
+      }
+    }
+    mockedAxios.get.mockResolvedValueOnce(mAxiosResponse)
+    const wrapper = await act(async () =>
+      renderComponent(
+        <Provider store={store}>
+          <Router>
+            <ProductList />
+          </Router>
+        </Provider>
+      )
+    )
+
+    const { getAllByTestId } = wrapper
+    const buttons = getAllByTestId('add-to-cart-btn')
+    expect(buttons).toHaveLength(3)
+    expect(getAllByTestId('add-to-cart-btn')[0].innerHTML).toBe('Add To Cart')
+    await act(async () => fireEvent.click(buttons[0]))
+    expect(getAllByTestId('add-to-cart-btn')[0].innerHTML).toBe('Remove From Cart')
+    wrapper.unmount()
   })
 })
